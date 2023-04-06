24c795e9dbfc6416df1fdbc2fb79908e5ea63a28
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-04-06 19:01:17 +0300

    Use Z- and S-curves

--- ai/controllers/fuzzy.py
+++ ai/controllers/fuzzy.py
@@ -10,8 +10,8 @@ class Fuzzy:
         self.x_distance = np.arange(0, 101, 1)
 
         # Fuzzy membership functions
-        self.dist_lo = fz.trapmf(self.x_distance, [0, 0, 10, 20])
-        self.dist_hi = fz.trapmf(self.x_distance, [10, 20, 100, 100])
+        self.dist_lo = fz.zmf(self.x_distance, 5, 15)
+        self.dist_hi = fz.smf(self.x_distance, 5, 15)
 
     def update(self, distance):
         self.distance = distance
@@ -20,4 +20,4 @@ class Fuzzy:
         return fz.interp_membership(self.x_distance, self.dist_hi, self.distance)
 
     def get_brake(self):
-        return fz.interp_membership(self.x_distance, self.dist_lo, self.distance)
+        return 0.6 * fz.interp_membership(self.x_distance, self.dist_lo, self.distance)
