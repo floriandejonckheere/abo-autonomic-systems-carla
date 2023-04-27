d284336c658d6533d97baaeb87d3f5dd947e2aa7
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-04-27 11:53:07 +0300

    Release brake when using handbrake

--- ai/actions/handbrake.py
+++ ai/actions/handbrake.py
@@ -10,5 +10,9 @@ class Handbrake(Action):
     def apply(self, control):
         control.throttle = 0.0
 
-        # Apply handbrake if speed is low enough
-        control.hand_brake = True if self.speed < 5.0 else False
+        # Release brake and apply handbrake if speed is low enough
+        if self.speed < 5.0:
+            control.brake = 0.0
+            control.hand_brake = True
+        else:
+            control.hand_brake = False
