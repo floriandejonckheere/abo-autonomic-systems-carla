e5aa77cf9624c80fb8a0f29c14b97cd34bca1d7a
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-04-27 11:55:37 +0300

    Add emergency brake action

--- ai/actions/__init__.py
+++ ai/actions/__init__.py
@@ -1,5 +1,6 @@
 from .accelerate import Accelerate
 from .brake import Brake
+from .emergency_brake import EmergencyBrake
 from .handbrake import Handbrake
 from .limit import Limit
 from .steer import Steer
---
+++ ai/actions/emergency_brake.py
@@ -0,0 +1,14 @@
+import numpy as np
+import skfuzzy as fz
+
+from .action import Action
+
+
+class EmergencyBrake(Action):
+    """Apply emergency brake"""
+
+    def apply(self, control):
+        # Set throttle and brake
+        control.throttle = 0.0
+        control.brake = 1.0
+        control.hand_brake = True  # Not usually a good idea in emergencies, but we need the breaking power
