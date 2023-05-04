5af4f60255f0aab6ab5f3251bb10a0b03f4d3c46
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-04 11:16:36 +0300

    Fix typo

--- ai/actions/emergency_brake.py
+++ ai/actions/emergency_brake.py
@@ -8,4 +8,4 @@ class EmergencyBrake(Action):
         # Set throttle and brake
         control.throttle = 0.0
         control.brake = 1.0
-        control.hand_brake = True  # Not usually a good idea in emergencies, but we need the breaking power
+        control.hand_brake = True  # Not usually a good idea in emergencies, but we need the braking power
