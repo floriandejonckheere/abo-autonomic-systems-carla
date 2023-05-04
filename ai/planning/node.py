6fb727e15b571637aa8dbf11fba3eb09e9bda34f
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-04 19:45:17 +0300

    Use linear combination with one modifier

--- ai/planning/node.py
+++ ai/planning/node.py
@@ -7,8 +7,8 @@ class Node:
     def __init__(self, waypoint):
         self.waypoint = waypoint
 
-        # Generate unique id for the node as linear combination of the coordinates
-        self.id = (3 * int(100 * waypoint.transform.location.x)) + (5 * int(100 * waypoint.transform.location.y))
+        # Generate unique id for the node using a linear combination of the coordinates
+        self.id = 1_000_000 * int(100 * waypoint.transform.location.x) + int(100 * waypoint.transform.location.y)
 
     def __eq__(self, other) -> bool:
         return self.id == other.id
