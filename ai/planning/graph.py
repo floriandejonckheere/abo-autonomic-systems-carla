dc7a7ccc29d5273815e58eb590409a3e58aed5bf
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-09 09:11:05 +0300

    Use correct starting/ending waypoints of topological path

--- ai/planning/graph.py
+++ ai/planning/graph.py
@@ -49,8 +49,10 @@ class Graph:
 
     # Return the shortest path between two waypoints in the graph
     def shortest_path(self, source, destination):
-        # Find the closest topological waypoints to the source and destination (same road and lane)
-        source = next(v for (u, v) in self.topology if v.road_id == source.road_id and v.lane_id == source.lane_id)
+        # Find the ending waypoint on the edge of the current lane
+        source = next(v for (u, v) in self.topology if u.road_id == source.road_id and u.lane_id == source.lane_id)
+
+        # Find the starting waypoint on the edge of the destination lane
         destination = next(u for (u, v) in self.topology if v.road_id == destination.road_id and v.lane_id == destination.lane_id)
 
         # Wrap waypoints in Node objects
