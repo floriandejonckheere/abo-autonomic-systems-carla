9e6d64045124096a58e22c3ea7d643971ca4e65a
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-09 20:11:41 +0300

    Set hand brake on cars

--- game/scenarios/lidar.py
+++ game/scenarios/lidar.py
@@ -25,8 +25,23 @@ class Lidar(Scenario):
             carla.Location(-10, 55, 1.8431),
         ]
 
+        def _on_collision(self, event):
+            if not self:
+                return
+            print('Collision with: ', event.other_actor.type_id)
+            if event.other_actor.type_id.split('.')[0] == 'vehicle':
+                print("Test FAILED")
+
         for location in locations:
             spawn_point = carla.Transform(location=location,  rotation=carla.Rotation(yaw=90))
 
             car = utils.try_spawn_random_vehicle_at(self.world, spawn_point)
             self.actors.append(car)
+
+            car.apply_control(carla.VehicleControl(brake=1.0, hand_brake=True))
+
+            bp = self.world.get_blueprint_library().find('sensor.other.collision')
+            sensor = self.world.spawn_actor(bp, carla.Transform(), attach_to=car)
+            self.actors.append(sensor)
+
+            sensor.listen(lambda event: _on_collision(car, event))
