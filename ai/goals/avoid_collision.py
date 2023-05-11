773d1e28f100304266e56bfe0a9334681e387956
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-11 08:27:47 +0300

    Always brake when avoiding

--- ai/goals/avoid_collision.py
+++ ai/goals/avoid_collision.py
@@ -11,23 +11,23 @@ class AvoidCollision(Goal):
     def actions(self):
         if self.knowledge.obstacle:
             return [
-                # Emergency brake to avoid the obstacle
+                # Emergency brake to avoid the obstacle in front
                 actions.EmergencyBrake(),
             ]
         if self.knowledge.obstacle_left:
             return [
-                # Steer right and brake to avoid the obstacle
+                # Steer right and brake to avoid the obstacle on the left
                 actions.Swerve(0.7),
                 actions.Brake(3.0),
             ]
         elif self.knowledge.obstacle_right:
             return [
-                # Steer left and brake to avoid the obstacle
+                # Steer left and brake to avoid the obstacle on the right
                 actions.Swerve(-0.7),
                 actions.Brake(3.0),
             ]
         else:
             return [
-                # No obstacle in sight, wait until healing timeout expires
-                actions.Brake(0.0),
+                # No obstacle in sight, brake until healing timeout expires
+                actions.Brake(1.0),
             ]
