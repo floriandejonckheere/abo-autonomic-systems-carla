7efc67ad78be3074d9cc8d01ddc797e987a450d3
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-16 10:36:56 +0300

    Update straight scenario

--- game/scenarios/straight.py
+++ game/scenarios/straight.py
@@ -7,6 +7,6 @@ class Straight(Scenario):
     """Straight ahead"""
 
     waypoints = [
-        carla.Location(37.2, 3.4, 1.8431),
-        carla.Location(218.2, 5.9, 1.8431),
+        carla.Location(37.2, 4.4, 1.8431),
+        carla.Location(100.2, 4.4, 1.8431),
     ]
