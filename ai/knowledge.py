aedba3e101c61e1d7e2d9c009346bc056d5bf388
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-18 09:18:26 +0300

    Add list of obstacles to knowledge

--- ai/knowledge.py
+++ ai/knowledge.py
@@ -51,6 +51,10 @@ class Knowledge(object):
         self.proximity_left = 0.0
         self.proximity_right = 0.0
 
+        self.obstacles = []
+        self.obstacles_left = []
+        self.obstacles_right = []
+
         self.obstacle = False
         self.obstacle_left = False
         self.obstacle_right = False
