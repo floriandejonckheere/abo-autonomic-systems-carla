057e968da2dbceb8b93212fbf755855e8130dc69
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-18 14:28:48 +0300

    Add PID controller for steering

--- ai/control/controller.py
+++ ai/control/controller.py
@@ -11,6 +11,7 @@ class Controller:
 
         # Instantiate PID controllers for continuous variables
         self.throttle_controller = PIDController(K_p=0.5, K_i=2.0, K_d=0.0)
+        self.steer_controller = PIDController(K_p=0.5, K_i=1.5, K_d=0.01)
 
     def control(self, control, dt):
         # Convert dt to seconds
@@ -18,3 +19,4 @@ class Controller:
 
         # Apply PID control
         control.throttle = np.clip(self.throttle_controller.control(control.throttle, self.vehicle.get_control().throttle, dt), 0.0, 1.0)
+        control.steer = np.clip(self.steer_controller.control(control.steer, self.vehicle.get_control().steer, dt), -0.7, 0.7)
