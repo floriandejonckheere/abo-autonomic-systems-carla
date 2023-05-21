fa79a854e0f86164e94a4d9261717b5a23d61be0
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-05-21 11:20:48 +0300

    Stop swerving when already stopped

--- ai/goals/avoid_collision.py
+++ ai/goals/avoid_collision.py
@@ -15,13 +15,13 @@ class AvoidCollision(Goal):
                 # Emergency brake to avoid the obstacle in front
                 actions.EmergencyBrake(),
             ]
-        if self.knowledge.obstacle_left:
+        if self.knowledge.obstacle_left and self.knowledge.speed > 1.0:
             return [
                 # Steer right and brake to avoid the obstacle on the left
                 actions.Swerve(actions.Swerve.Right, self.knowledge.proximity_left.average()),
                 actions.Brake(3.0),
             ]
-        elif self.knowledge.obstacle_right:
+        elif self.knowledge.obstacle_right and self.knowledge.speed > 1.0:
             return [
                 # Steer left and brake to avoid the obstacle on the right
                 actions.Swerve(actions.Swerve.Left, self.knowledge.proximity_right.average()),
